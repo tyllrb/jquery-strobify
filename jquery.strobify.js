52121104b476dd26738bfb922307ece62ee31f4d
nynex97 <tyler.34@hotmail.com>
2013-12-03 15:25:52 -0500

    Create jquery.strobify.js
    initial commit

---
+++ jquery.strobify.js
@@ -0,0 +1,33 @@
+(function ($) {
+	var interval = null;
+	var pointer = 0;
+	var originalBackground = null;
+
+	$.fn.strobify = function (state, options) {
+
+		var settings = {
+			colors: ['#ffffff', '#000000'],
+			speed: 100
+		};
+
+		if (options !== null)
+			settings = $.extend(settings, options);
+
+		dom = this;
+
+		if (state == 'on') {
+			originalBackground = dom.css ('background');
+
+			interval = setInterval (function () {
+				pointer = (pointer >= (settings.colors.length - 1)) ? 0 : pointer + 1;
+				console.log ("pointer -> " + pointer);
+				dom.css ('background', settings.colors [pointer]);
+			}, settings.speed);
+		}
+
+		else if (state == 'off') {
+			clearInterval (interval);
+			dom.css ('background', originalBackground);
+		}
+	};
+}(jQuery));
